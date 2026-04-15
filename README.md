0c13688291e3d2f7c7b3c983c28259c1d3fc4a2a
oopsmails <oopsmails@gmail.com>
2026-04-15 11:02:04 -0400

    added controller and h2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 150-ai-test-java
+
+http://localhost:8080/ai-test-java/hello
+
+
+
--- src/main/java/com/example/aigen/controller/HelloController.java
+++ src/main/java/com/example/aigen/controller/HelloController.java
@@ -1,9 +1,11 @@
-package com.example.aigen;
+package com.example.aigen.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/${spring.application.name}")
 public class HelloController {
 
     @GetMapping("/hello")
