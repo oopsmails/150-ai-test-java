866a56a5790e24090cac38e11953802c3397a130
oopsmails <oopsmails@gmail.com>
2026-04-15 10:11:39 -0400

    generated simple springboot app

---
+++ src/main/java/com/example/aigen/AiGenApplication.java
@@ -0,0 +1,13 @@
+package com.example.aigen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiGenApplication.class, args);
+    }
+
+}
